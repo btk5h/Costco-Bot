c099556c1a8ea5a45e55650efe3a2b36f64869d4
Bryan Terce <bryan.terce@gmail.com>
2018-10-11 14:13:09 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const { CommandClient } = require("eris");
+const requireDir = require("require-dir");
+
+const { serverLogger, createLogger } = require("./lib/logging");
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+if (!BOT_TOKEN) {
+  serverLogger.error("The environment variable BOT_TOKEN is not set");
+} else {
+  startBot();
+}
+
+function startBot() {
+  serverLogger.info("Starting bot...");
+
+  const bot = new CommandClient(
+    BOT_TOKEN,
+    {},
+    {
+      prefix: "!"
+    }
+  );
+
+  serverLogger.info("Loading modules...");
+
+  Object.entries(requireDir("./lib/modules"))
+    .forEach(([file, module]) => {
+      serverLogger.info(`Loading module ${file}`);
+      module({ bot, log: createLogger(file) });
+    });
+
+  bot.connect();
+}
+
