0645defa27bac8d9422c5549bc93224045fcf94c
Bryan Terce <bryan.terce@gmail.com>
2018-12-27 02:08:19 -0800

    help

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 pm2 start /dev/app/app.js
 pm2 save
--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 pm2 stop all
 pm2 delete all
