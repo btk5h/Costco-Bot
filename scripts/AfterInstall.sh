189c74ea888c6a3fc16efde0c3dbb699bd1cbeed
Bryan Terce <bryan.terce@gmail.com>
2018-12-27 01:09:28 -0800

    Give up on root

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-source /home/ec2-user/.bash_profile
-
 cd /var/app
 yarn install
--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-source /home/ec2-user/.bash_profile
-
 pm2 start /dev/app/app.js
 pm2 save
